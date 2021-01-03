e20e8ab5d09fa757640baa28c5aee1a728e281ee
lostintime <lostintime.dev@gmail.com>
2021-01-03 15:54:15 +0200

    Rename SomeApi type alias in test

--- test/breaking.test.ts
+++ test/breaking.test.ts
@@ -1,11 +1,11 @@
 import { expect } from "chai"
 import { SomeApi } from "../src"
-import { SomeApi as SomeApiV1 } from "@lostintime/some-api-base"
+import { SomeApi as SomeApiBase } from "@lostintime/some-api-base"
 
 describe("API", () => {
   describe("Breaking Changes", () => {
     it("didn't happen yet", () => {
-      const useApi = (newApi: SomeApi): SomeApiV1 => newApi
+      const useApi = (newApi: SomeApi): SomeApiBase => newApi
       // Breaking changes test is coming here, after first version is published
       expect(useApi).is.a("function")
     })
