7d1ea71de77f8ee4cf3be673927d6de4e33db2a7
lostintime <lostintime.dev@gmail.com>
2021-01-03 14:40:24 +0200

    Add .npmignore

--- test/breaking.test.ts
+++ test/breaking.test.ts
@@ -1,10 +1,13 @@
 import { expect } from "chai"
+import { SomeApi } from "../src"
+import { SomeApi as SomeApiV1 } from "@lostintime/some-api-base"
 
 describe("API", () => {
   describe("Breaking Changes", () => {
     it("didn't happen yet", () => {
+      const useApi = (newApi: SomeApi): SomeApiV1 => newApi
       // Breaking changes test is coming here, after first version is published
-      expect(true).not.equals(false, "there is only one true")
+      expect(useApi).is.a("funtion")
     })
   })
 })
