090f0ecb23d7933c2d18959100012dd570430725
lostintime <lostintime.dev@gmail.com>
2021-01-03 15:56:44 +0200

    Cleanup some comments

--- test/breaking.test.ts
+++ test/breaking.test.ts
@@ -6,7 +6,8 @@ describe("API", () => {
   describe("Breaking Changes", () => {
     it("didn't happen yet", () => {
       const useApi = (newApi: SomeApi): SomeApiBase => newApi
-      // Breaking changes test is coming here, after first version is published
+      // This asertion is just to satisfy linter that useApi is used
+      // Otherwise useApi signature is all we care about for version compatibility test
       expect(useApi).is.a("function")
     })
   })
