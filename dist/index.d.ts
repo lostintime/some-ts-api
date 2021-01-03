ba20427e9e9c04d3b4e5ab98b204c2b656bb01b3
lostintime <lostintime.dev@gmail.com>
2021-01-03 14:52:34 +0200

    Build

---
+++ dist/index.d.ts
@@ -0,0 +1,14 @@
+interface Fruit {
+    readonly cat: "fruit";
+}
+interface Juice {
+    readonly cat: "juice";
+}
+interface Jam {
+    readonly cat: "jam";
+}
+export interface SomeApi {
+    makeJuice(fruit: Fruit): Promise<Juice>;
+    makeJam(fruit: Fruit): Promise<Jam>;
+}
+export {};
